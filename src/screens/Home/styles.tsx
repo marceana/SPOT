5f2f0fc49da1ca1dffee95cde11e99a7d52364f5
Marce <assumpcaom23@gmail.com>
2023-09-30 23:35:35 -0300

    feat: adiciona visual inicial da home page

---
+++ src/screens/Home/styles.tsx
@@ -0,0 +1,49 @@
+import { StyleSheet } from 'react-native';
+import { Dimensions } from 'react-native';
+
+const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: 500,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 50,
+    fontWeight: 'bold',
+    color: 'white',
+    width: '70%',
+    marginLeft: 25,
+  },
+  button: {
+    backgroundColor: '#fff',
+    width: 200,
+    marginLeft: 25,
+    marginTop: 20,
+    height: 40,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#000',
+  },
+  searchButton: {
+    backgroundColor: '#fff',
+    height: 60,
+    width: Dimensions.get('screen').width - 20,
+    borderRadius: 30,
+    marginHorizontal: 10,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    top: 20,
+    zIndex: 100,
+    alignSelf: 'center',
+  },
+});
+
+export default styles;
