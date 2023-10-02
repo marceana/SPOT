6faab99cae37dd47f2b9e630fb37a219162d13e8
Marce <assumpcaom23@gmail.com>
2023-10-02 15:09:55 -0300

    style: estilos do componente de post

---
+++ src/components/Post/styles.tsx
@@ -0,0 +1,37 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 20,
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 3 / 2,
+    resizeMode: 'cover',
+    borderRadius: 20,
+  },
+  garages: {
+    marginVertical: 10,
+    color: '#5b5b5b',
+  },
+  description: {
+    fontSize: 18,
+    lineHeight: 26,
+  },
+  prices: {
+    fontSize: 18,
+  },
+  oldPrice: {
+    color: '#5b5b5b',
+    textDecorationLine: 'line-through',
+  },
+  price: {
+    fontWeight: 'bold',
+  },
+  totalPrice: {
+    color: '#5b5b5b',
+    textDecorationLine: 'underline',
+  },
+});
+
+export default styles;
