eb2fea9aa4403f70a6a64811932510eaa232ec88
Marce <assumpcaom23@gmail.com>
2023-10-06 09:14:59 -0300

    feat: altera o app pra chamar o Router

--- App.tsx
+++ App.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
-
+import 'react-native-gesture-handler';
 import HomeScreen from './src/screens/Home';
+import Router from './src/navigation/Router';
 
 function App(): JSX.Element {
-  return (
-    <SafeAreaView>
-      <HomeScreen />
-    </SafeAreaView>
-  );
+  return <Router />;
 }
 
 export default App;
